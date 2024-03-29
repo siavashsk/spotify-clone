6194bdc8656517092142631ca155e9c1ff705d37
siavash <siavash.khani200@gmail.com>
2024-03-29 14:58:49 +0330

    feat(useGetSongById): finished second hook for player func

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import UserProvider from "@/providers/userProvider";
 import ModalProvider from "@/providers/modalProvider";
 import ToastProvider from "@/providers/ToasterProvider";
 import getSongsByUserId from "@/actions/getSongsByUserId";
+import Player from "@/components/player";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default async function RootLayout({
           <UserProvider>
             <ModalProvider />
             <Sidebar songs={userSongs}>{children}</Sidebar>
+            <Player />
           </UserProvider>
         </SupabaseProvider>
       </body>
---
+++ components/player.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import usePlayer from "@/hooks/usePlayer";
+
+const Player = () => {
+  const player = usePlayer();
+    
+
+  return <div>player</div>;
+};
+
+export default Player;
