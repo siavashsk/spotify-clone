3d968ff77595f49668ace26fae9c116be3d63f68
siavash <siavash.khani200@gmail.com>
2024-03-29 16:01:56 +0330

    feat(player): implemented player component onClick

--- app/(site)/components/pageContent.tsx
+++ app/(site)/components/pageContent.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import SongItem from "@/components/songItem";
+import useOnPlay from "@/hooks/useOnPlay";
 import { Song } from "@/types";
 
 interface Props {
@@ -8,6 +9,8 @@ interface Props {
 }
 
 const PageContent: React.FC<Props> = ({ songs }) => {
+  const onPlay = useOnPlay(songs);
+
   if (songs.length === 0) {
     return <div className="mt-4 text-neutral-400">No songs available </div>;
   }
@@ -15,7 +18,13 @@ const PageContent: React.FC<Props> = ({ songs }) => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4">
       {songs.map((item) => (
-        <SongItem key={item.id} onClick={() => {}} data={item} />
+        <SongItem
+          key={item.id}
+          onClick={(id: string) => {
+            onPlay(id);
+          }}
+          data={item}
+        />
       ))}
     </div>
   );
--- components/player.tsx
+++ components/player.tsx
@@ -1,12 +1,24 @@
 "use client";
 
+import useGetSongById from "@/hooks/useGetSongById";
+import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 import usePlayer from "@/hooks/usePlayer";
 
 const Player = () => {
   const player = usePlayer();
-    
+  const { song } = useGetSongById(player.activeId);
 
-  return <div>player</div>;
+  const songUrl = useLoadSongUrl(song!);
+
+  if (!song || !songUrl || !player.activeId) {
+    return null;
+  }
+
+  return (
+    <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
+      player
+    </div>
+  );
 };
 
 export default Player;
